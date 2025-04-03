67ea74b7e76f310bf856995249b77d82d4f190bd
Shema Manasseh <shemamanase992@gmail.com>
2025-04-03 23:12:49 +0300

    Add Stripe PHP library version 17.1.0 and update autoload files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '76cd962e97d842cd6d8726395ebb46ac4d00bb07',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '76cd962e97d842cd6d8726395ebb46ac4d00bb07',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'stripe/stripe-php' => array(
+            'pretty_version' => 'v17.1.0',
+            'version' => '17.1.0.0',
+            'reference' => 'b4ce1fdd4ec607e4e8906419c346846f19363f10',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stripe/stripe-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
